8ac2cc6647e8cac6de58eb8af0e57415af714808
toydayo <kawamataro1209@gmail.com>
2026-08-14 20:06:04 +0900

    controller_canbridge.ymlファイルの作成

--- src/nhk2026_bridge/launch/dansa_arm_can.launch.py
+++ src/nhk2026_bridge/launch/dansa_arm_can.launch.py
@@ -57,7 +57,7 @@ def _ensure_can0_up(context, *args, **kwargs):
 def generate_launch_description():
     # パス設定
     pkg_share_bridge = get_package_share_directory('nhk2026_bridge')
-    canid_file = os.path.join(pkg_share_bridge, 'config', 'step_leg_canbridge.yml')
+    canid_file = os.path.join(pkg_share_bridge, 'config', 'controller_canbridge.yml')
 
     ld = LaunchDescription()
     
