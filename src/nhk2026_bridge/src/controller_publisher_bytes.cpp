ee34b2c5b55b05ce903d7ab6d3975ff302025643
toydayo <kawamataro1209@gmail.com>
2026-08-15 15:58:10 +0900

    canの送信方法変更

--- src/nhk2026_bridge/src/controller_publisher_bytes.cpp
+++ src/nhk2026_bridge/src/controller_publisher_bytes.cpp
@@ -1,24 +1,30 @@
-#include <rclcpp/rclcpp.hpp>
-#include <std_msgs/msg/u_int8_multi_array.hpp>
-
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/u_int8_multi_array.hpp"
+#include <vector>
 using namespace std::chrono_literals;
 
 class BytePublisher : public rclcpp::Node {
 public:
   BytePublisher() : Node("controller_byte_publisher") {
-    pub_ = this->create_publisher<std_msgs::msg::UInt8MultiArray>("motor0_cmd", 10);
+    pub0_ = this->create_publisher<std_msgs::msg::UInt8MultiArray>("motor0_cmd", 10);
+    pub1_ = this->create_publisher<std_msgs::msg::UInt8MultiArray>("motor1_cmd", 10);
+    pub2_ = this->create_publisher<std_msgs::msg::UInt8MultiArray>("motor2_cmd", 10);
     timer_ = this->create_wall_timer(1s, std::bind(&BytePublisher::on_timer, this));
   }
 private:
   void on_timer() {
-    // 例: speed_target=1000.0 (LE float 00 00 7A 44), pid_mode=1, ctrl_mode=0
-    std::vector<uint8_t> data = {0,0,122,68,1,0,0,0};
+    // 例: vx = 100 mm/s -> 0x0064 -> [0x64,0x00], 他は0, pid_mode=1, ctrl=0
+    std::vector<uint8_t> data = {0x64,0x00, 0x00,0x00, 0x00,0x00, 0x01,0x00};
     auto msg = std::make_shared<std_msgs::msg::UInt8MultiArray>();
     msg->data = data;
-    pub_->publish(*msg);
-    RCLCPP_INFO(this->get_logger(), "published %zu bytes", data.size());
+    pub0_->publish(*msg);
+    pub1_->publish(*msg);
+    pub2_->publish(*msg);
+    RCLCPP_INFO(this->get_logger(), "published %zu bytes to 3 topics", data.size());
   }
-  rclcpp::Publisher<std_msgs::msg::UInt8MultiArray>::SharedPtr pub_;
+  rclcpp::Publisher<std_msgs::msg::UInt8MultiArray>::SharedPtr pub0_;
+  rclcpp::Publisher<std_msgs::msg::UInt8MultiArray>::SharedPtr pub1_;
+  rclcpp::Publisher<std_msgs::msg::UInt8MultiArray>::SharedPtr pub2_;
   rclcpp::TimerBase::SharedPtr timer_;
 };
 
